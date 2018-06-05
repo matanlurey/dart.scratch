d0a33aaf48cc93f2a67d2374b0c81e7b00004217
Matan Lurey <matan@lurey.org>
2018-06-04 18:16:33 -0700

    Add example of async function mischief.

---
+++ web_compilers_async/web/main.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:meta/dart2js.dart' as dart2js;
+
+void main() {
+  runZoned(() {
+    new AsyncDoEvent().doEvent();
+  }, zoneSpecification: new ZoneSpecification(
+    scheduleMicrotask: (self, parent, zone, fn) {
+      print('scheduleMicrotask()');
+      return parent.scheduleMicrotask(zone, fn);
+    },
+  ));
+}
+
+abstract class HasEvent {
+  void doEvent();
+}
+
+class AsyncDoEvent implements HasEvent {
+  @dart2js.noInline
+  void doEvent() async {}
+}
